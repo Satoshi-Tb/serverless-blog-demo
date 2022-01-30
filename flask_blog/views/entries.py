6b7dfea616e590a613276238c0b69d24ffa8d7c6
Satoshi T <sulp91_fy@yahoo.co.jp>
2022-01-31 00:53:15 +0900

    PynamoDBãによるCRUD実装

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,35 +1,38 @@
-from flask import request, redirect, url_for, render_template, flash, session
+import flask
+from flask import request, redirect, url_for, render_template, flash, session, Markup
 from flask_blog import app
 from datetime import datetime
+from flask_blog.models.entries import Entry
+
+
+# 改行コードを<br>に置換し、その他のhtml特殊文字をエスケープしたい
+@app.template_filter("cr")
+def cr(arg):
+    return Markup(arg.replace("\r\n", "<br>"))
 
 
 @app.route("/")
 def show_entries():
-    entries = [
-        {
-            "id": 1,
-            "title": "はじめての投稿",
-            "text": "はじめての内容",
-            "created_at": datetime.now(),
-        },
-        {
-            "id": 2,
-            "title": "2つ目の投稿",
-            "text": "２つ目の内容",
-            "created_at": datetime.now(),
-        },
-    ]
+
+    entries = Entry.scan()
+    entries = sorted(entries, key=lambda x: x.article_id, reverse=True)
 
     user = {
         "is_authenticated": True
     }
-
     return render_template("entries/index.html", entries=entries, current_user=user)
 
 
 @app.route("/entries", methods=["POST"])
 def add_entry():
-    return "新しく記事が作成されました"
+    entry = Entry(
+        article_id=int(datetime.now().timestamp()),
+        title=request.form["title"],
+        text=request.form["text"],
+    )
+    entry.save()
+
+    return redirect(url_for("show_entries"))
 
 
 @app.route("/entries/new", methods=["GET"])
@@ -43,13 +46,7 @@ def new_entry():
 
 @app.route("/entries/<int:article_id>", methods=["GET"])
 def show_entry(article_id):
-    entry = {
-        "id": 1,
-        "title": "はじめての投稿",
-        "text": "はじめての内容",
-        "created_at": datetime.now(),
-    }
-
+    entry = Entry.get(article_id)
     user = {
         "is_authenticated": True
     }
@@ -59,13 +56,7 @@ def show_entry(article_id):
 
 @app.route("/entries/<int:article_id>/edit", methods=["GET"])
 def edit_entry(article_id):
-    entry = {
-        "id": 1,
-        "title": "はじめての投稿",
-        "text": "はじめての内容",
-        "created_at": datetime.now(),
-    }
-
+    entry = Entry.get(article_id)
     user = {
         "is_authenticated": True
     }
@@ -75,9 +66,19 @@ def edit_entry(article_id):
 
 @app.route("/entries/<int:article_id>/update", methods=["POST"])
 def update_entry(article_id):
-    return f"記事{article_id}を更新しました"
+    entry = Entry.get(article_id)
+
+    entry.update(actions=[
+        Entry.title.set(request.form["title"]),
+        Entry.text.set(request.form["text"]),
+    ])
+
+    return redirect(url_for("show_entries"))
 
 
 @app.route("/entries/<int:article_id>/delete", methods=["POST"])
 def delete_entry(article_id):
-    return f"記事{article_id}を削除しました"
+    entry = Entry.get(article_id)
+    entry.delete()
+
+    return redirect(url_for("show_entries"))
